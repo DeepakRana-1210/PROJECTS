1d13fec73b6a8c5ce695ff14ce1a60aa07494f9f
DeepakRana-1210 <78957217+DeepakRana-1210@users.noreply.github.com>
2021-02-17 23:03:30 +0530

    Add files via upload

---
+++ 13/basic.js
@@ -0,0 +1,49 @@
+function main1()
+{
+document.getElementById('miniimg').src="Images/1.jpg";
+}
+
+function main2()
+{
+document.getElementById('miniimg').src="Images/2.jpg";
+}
+
+function main3()
+{
+document.getElementById('miniimg').src="Images/3.jpg";
+}
+
+function main4()
+{
+document.getElementById('miniimg').src="Images/4.jpg";
+}
+
+function main5()
+{
+document.getElementById('miniimg').src="Images/5.jpg";
+}
+
+function main6()
+{
+document.getElementById('miniimg').src="Images/6.jpg";
+}
+
+function main7()
+{
+document.getElementById('miniimg').src="Images/7.jpg";
+}
+
+function main8()
+{
+document.getElementById('miniimg').src="Images/8.jpg";
+}
+
+function main9()
+{
+document.getElementById('miniimg').src="Images/9.jpg";
+}
+
+function main10()
+{
+document.getElementById('miniimg').src="Images/10.jpg";
+}
